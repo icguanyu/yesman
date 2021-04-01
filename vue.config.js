19a1dfd710d2e07c3eb92288702d411114fc52e6
Nick <icguanyu@gmail.com>
2021-04-01 14:53:38 +0800

    seo 更新

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,11 @@
 var webpack = require("webpack");
 module.exports = {
- 
   pages: {
     index: {
       entry: "src/main.js",
       template: "public/index.html",
       filename: "index.html",
-      title: "Yes Man 專業攝影工作室",
+      title: "Yes man專業攝影工作室-兒童寫真,孕婦寫真,新生兒到府拍攝",
     },
   },
   css: {
