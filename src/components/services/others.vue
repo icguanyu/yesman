456523a816412b19e8b9cd0b37a5c336d33ae2a1
Nick <icguanyu@gmail.com>
2021-04-07 23:33:55 +0800

    內容修改

--- src/components/services/others.vue
+++ src/components/services/others.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!--其他加購-->
-    <div v-if="filter(['h'])">
+    <div v-if="filter(['h','i'])">
       <el-divider content-position="left">其他加購項目</el-divider>
       <div class="others">
         <table v-if="filter(['h'])">
@@ -30,27 +30,23 @@
             </tr>
             <tr v-if="filter(['a', 'b'])">
               <td>$1000/人</td>
-              <td>多一位小朋友拍攝（含2套服裝搭配2種造型）<span>註2</span></td>
+              <td>多一位小朋友拍攝（含2套服裝搭配2種造型）<span>(註2)</span></td>
             </tr>
             <tr v-if="filter(['e', 'f', 'g', 'h', 'i'])">
               <td>$500/$800</td>
               <td>彌月卡（50/100張）</td>
             </tr>
-            <tr v-if="filter(['e', 'f', 'g', 'h', 'i'])">
-              <td>$500/人</td>
-              <td>多一位大人入鏡拍攝（不含服裝造型）</td>
-            </tr>
           </tbody>
         </table>
         <div class="others-notes">
-          <li>註1:服裝僅提供黑色西裝以及禮服</li>
-          <li>註2:小朋友年齡限制為0-11歲，滿12(含)歲以上算大人</li>
-          <p style="color: red; font-size: 18px; margin: 10px 0">
-            拍攝成品需自取，如需郵寄宅配到府貨，需自行負擔郵費
-          </p>
+          <li v-if="filter(['a','b'])">註1:服裝僅提供黑色西裝以及禮服</li>
+          <li v-if="filter(['a','b'])">註2:小朋友年齡限制為0-11歲，滿12(含)歲以上算大人</li>
         </div>
       </div>
     </div>
+    <p style="color: red; font-size: 18px; margin: 10px 0">
+      拍攝成品需自取，如需郵寄宅配到府貨，需自行負擔郵費
+    </p>
     <!--拍攝注意事項-->
     <el-collapse v-model="activeCollapse">
       <el-collapse-item name="0" v-if="filter(['c', 'd', 'e', 'f', 'g', 'h', 'i'])">
@@ -66,7 +62,10 @@
               拍攝完成後，修片時間需<b>三</b>週，如需製作彌月卡，請事先挑選一張照片製作。
             </li>
             <li>
-              修片完成會以「雲端方式」先行選產品，挑選<b>30</b>張照片入本
+              修片完成會以「雲端方式」先行選產品，挑選
+              <b v-if="id=='a'">30</b>
+              <b v-else>20</b>
+              張照片入本
               <span v-if="filter(['b'])">；再挑選<b>1</b>張做成相框(橫豎不限)</span>
               。以上照片挑選都必須提供編號給我們，避免挑選錯誤。
             </li>
@@ -82,7 +81,7 @@
         </template>
         <div class="content">
           <ol>
-            <li>
+            <li v-if="filter(['i'])">
               大人若是入鏡拍攝，需自備一套服裝，和小朋友其中一套服裝做拍攝，如需和小朋友兩套服裝做拍攝，需加價一套$500/人。
             </li>
             <li>本公司全程<b>禁止側拍側錄</b>。</li>
@@ -96,7 +95,7 @@
           </ol>
         </div>
       </el-collapse-item>
-      <el-collapse-item name="2" v-if="filter(['a', 'b', 'c', 'd'])">
+      <el-collapse-item name="2" v-if="filter(['a', 'b', 'c', 'd','h','i'])">
         <template slot="title">
           <div class="collspse-title">
             <i class="header-icon el-icon-info"></i>貼心提醒
@@ -115,6 +114,23 @@
           </ol>
         </div>
       </el-collapse-item>
+      <!--START:證件照貼心提醒-->
+      <el-collapse-item name="2" v-if="filter(['a', 'b', 'c', 'd','e','f','g','i'])">
+        <template slot="title">
+          <div class="collspse-title">
+            <i class="header-icon el-icon-info"></i>貼心提醒
+          </div>
+        </template>
+        <div class="content">
+          <ol>
+            <li>如果生病身體不舒服精神欠佳，請主動告知並延期拍攝。</li>
+            <li>身上不要有任何紋身貼紙、蓋章等記號。</li>
+            <li>不能配戴有色隱形眼鏡。</li>
+            <li>不能配戴粗框、會反光的眼鏡</li>
+          </ol>
+        </div>
+      </el-collapse-item>
+      <!--END:證件照貼心提醒-->
       <el-collapse-item name="3" v-if="filter(['e', 'f', 'g', 'h', 'i'])">
         <template slot="title">
           <div class="collspse-title">
